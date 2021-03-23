bc3dc957d07573ff6d8e495a5813e59d1aaad535
eniompw <71254710+eniompw@users.noreply.github.com>
2021-03-23 17:55:11 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,16 @@
-# A very simple Flask Hello World app for you to get started with...
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def msg():
+    return render_template('msg.html')
 
-@app.route('/save')
-def save():
+@app.route('/read')
+def read():
     msg = request.args.get('msg', '')
-    f = open("messages.txt", "a")
-    f.write(msg + '<br>')
-    f.close()
-    f = open("messages.txt", "r")
+    if msg != '':
+        f = open("file.txt", "a")
+        f.write(msg + '<br>')
+        f.close()
+    f = open("file.txt", "r")
     return f.read()
