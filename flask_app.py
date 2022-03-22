477ae2f930e08044d7773a2d0c61c68a098c7de7
eniompw <71254710+eniompw@users.noreply.github.com>
2022-03-22 09:43:26 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def msg():
-    return render_template('msg.html')
-
-@app.route('/read')
-def read():
-    msg = request.args.get('msg', '')
-    if msg != '':
+@app.route("/")
+def home():
+    msg = request.args.get('msg','')
+    if msg == '':
+        f = open("file.txt", "r")
+        return "<form>Message <input name='msg'></form>" + f.read()
+    else:
         f = open("file.txt", "a")
         f.write(msg + '<br>')
         f.close()
-    f = open("file.txt", "r")
-    return f.read()
+        f = open("file.txt", "r")
+        return "<form>Message <input name='msg'></form>" + f.read()
